bf6a038e332c0b2bbd9900e599f4333c42c7cc7e
Thiago <thiagolima1011@gmail.com>
2021-02-01 18:42:00 -0300

    Rest testado!

---
+++ src/main/java/com/thiagomoura/musicmc/MusicApplication.java
@@ -0,0 +1,13 @@
+package com.thiagomoura.musicmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicApplication.class, args);
+	}
+
+}
